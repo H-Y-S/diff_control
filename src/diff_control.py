93f25af4f378aca4ac9e02e01ef29e993dfa5e15
Heikki Suhonen <heikki.suhonen@gmail.com>
2015-06-29 09:51:12 +0300

    further modifications of the ui

--- src/diff_control.py
+++ src/diff_control.py
@@ -19,8 +19,29 @@ NUM_INTEGER_FIELD_NAMES = ['mot1_step_entry','mot2_step_entry',
 FILENAME_CHARS = "-_%s%s" % (string.ascii_letters,string.digits)
 CONFIG_FILE_NAME = 'config.ini'
 
+#MOT1_LABEL1 = ['Z Start [mm]','Y Start [mm]','Rot Start [deg]','Z Start [mm]','Rot Start [deg]','Z Start [mm]']
+#MOT1_LABEL2 = ['Z End [mm]','Y End [mm]','Rot End [deg]','Z End [mm]','Rot End [deg]','Z End [mm]']
+#MOT1_LABEL3 = ['Z Steps','Y Steps','Rot Steps','Z Steps','Rot Steps','Z Steps']
+
+
+SCAN_TYPE_MOT1_NAMES = ['Z','Y','Rot','Z','Rot','Z']
+SCAN_TYPE_MOT2_NAMES = ['','','','Y','Z','Rot']
+SCAN_TYPE_MOT1_UNITS = ['mm','mm','deg','mm','deg','mm']
+SCAN_TYPE_MOT2_UNITS = ['','','','mm','mm','deg']
+
 
 class DiffControl:
+    # This one is called when the main window is shown
+    def on_main_window_show(self, widget):
+        # If you return FALSE in the "delete_event" signal handler,
+        # GTK will emit the "destroy" signal. Returning TRUE means
+        # you don't want the window to be destroyed.
+        # This is useful for popping up 'are you sure you want to quit?'
+        # type dialogs.
+        print "show event occurred"
+        self.update_view()
+
+
     # This one is called when the main window close-button is clicked
     def on_main_window_delete_event(self, widget, event, data=None):
         # If you return FALSE in the "delete_event" signal handler,
@@ -35,6 +56,7 @@ class DiffControl:
         # TODO: Confirm close
         return False
 
+    
     # This one is called when the main window is destroyed (i.e. when  
     # delete_event returns null)
     def on_main_window_destroy(self, widget, data=None):
@@ -66,8 +88,10 @@ class DiffControl:
 
         entry = self.builder.get_object('acq_filename_entry')
         self.constrain_filename(entry)
-        
+        self.update_view()
 
+
+        
     # start the event-loop (and end of control here)
     def run(self):
         self.builder.get_object("main_window").show_all()
@@ -162,7 +186,8 @@ class DiffControl:
 
         self.mFileNamePrefix = self.builder.get_object('acq_filename_entry').get_text()
 
-        self.update_summary_fields()
+        # update the view
+        self.update_view()
 
 
     def convert_text_to_float(self,text):
@@ -193,8 +218,8 @@ class DiffControl:
         self.builder.get_object('acq_img_number_entry').set_text(str(self.mAcqCount))
         self.builder.get_object('acq_filename_entry').set_text(self.mFileNamePrefix)
 
-        self.update_summary_fields()
-        self.update_control_buttons()
+        # Update the view
+        self.update_view()
 
 
 
@@ -202,32 +227,51 @@ class DiffControl:
         # Set the labels for motors and hide motor2 controls if necessary
         if self.mScanType in [0,1,2] :
             # Hide motor 2 fields
-            self.builder.get_object('mot2_label1').set_visible(False)
-            self.builder.get_object('mot2_label2').set_visible(False)
-            self.builder.get_object('mot2_label3').set_visible(False)
-            self.builder.get_object('mot2_step_size').set_visible(False)
-            self.builder.get_object('mot2_start_entry').set_visible(False)
-            self.builder.get_object('mot2_end_entry').set_visible(False)
-            self.builder.get_object('mot2_step_entry').set_visible(False)
-            self.builder.get_object('slow_axis_label').set_visible(False)
-            self.builder.get_object('fast_axis_label').set_visible(False)
-
+            self.builder.get_object('mot2_label1').hide()
+            self.builder.get_object('mot2_label2').hide()
+            self.builder.get_object('mot2_label3').hide()
+#            self.builder.get_object('mot2_step_size').hide()
+            self.builder.get_object('mot2_start_entry').hide()
+            self.builder.get_object('mot2_end_entry').hide()
+            self.builder.get_object('mot2_step_entry').hide()
+            self.builder.get_object('slow_axis_label').hide()
+            self.builder.get_object('fast_axis_label').hide()
+            
         else :
             # Show motor 2 fields
-            self.builder.get_object('mot2_label1').set_visible(True)
-            self.builder.get_object('mot2_label2').set_visible(True)
-            self.builder.get_object('mot2_label3').set_visible(True)
-            self.builder.get_object('mot2_step_size').set_visible(True)
-            self.builder.get_object('mot2_start_entry').set_visible(True)
-            self.builder.get_object('mot2_end_entry').set_visible(True)
-            self.builder.get_object('mot2_step_entry').set_visible(True)
-            self.builder.get_object('slow_axis_label').set_visible(True)
-            self.builder.get_object('fast_axis_label').set_visible(True)
+            self.builder.get_object('mot2_label1').show()
+            self.builder.get_object('mot2_label2').show()
+            self.builder.get_object('mot2_label3').show()
+ #           self.builder.get_object('mot2_step_size').show()
+            self.builder.get_object('mot2_start_entry').show()
+            self.builder.get_object('mot2_end_entry').show()
+            self.builder.get_object('mot2_step_entry').show()
+            self.builder.get_object('slow_axis_label').show()
+            self.builder.get_object('fast_axis_label').show()
 
 
         # Then change the labels
-        
-        
+        mot = SCAN_TYPE_MOT1_NAMES[self.mScanType]
+        unit = SCAN_TYPE_MOT1_UNITS[self.mScanType]
+        self.builder.get_object('mot1_label1').set_text(mot + ' Start [' + unit + ']')
+        self.builder.get_object('mot1_label2').set_text(mot + ' End [' + unit + ']')
+        self.builder.get_object('mot1_label3').set_text(mot + ' Steps')
+
+        mot = SCAN_TYPE_MOT2_NAMES[self.mScanType]
+        unit = SCAN_TYPE_MOT2_UNITS[self.mScanType]
+        self.builder.get_object('mot2_label1').set_text(mot + ' Start [' + unit + ']')
+        self.builder.get_object('mot2_label2').set_text(mot + ' End [' + unit + ']')
+        self.builder.get_object('mot2_label3').set_text(mot + ' Steps')
+
+
+
+    # Does all view updates
+    def update_view(self):
+        self.update_control_buttons()
+        self.set_labels_and_hideshow_fields()
+        self.update_summary_fields()
+
+                
     def update_summary_fields(self):
         n1 = self.mMot1Step
         n2 = self.mMot2Step
@@ -250,9 +294,12 @@ class DiffControl:
             
         mot1size = self.calc_mot1_step_size()
         mot2size = self.calc_mot2_step_size()
-        self.builder.get_object('mot1_step_size').set_text('Step size: ' + '%.2f' % mot1size + u'\N{DEGREE SIGN}')
-        self.builder.get_object('mot2_step_size').set_text('Step size: ' + '%.3f' % mot2size + ' mm')
-        
+#        u'\N{DEGREE SIGN}'
+        self.builder.get_object('mot1_step_size').set_text('Step size: ' + '%.2f ' % mot1size + SCAN_TYPE_MOT1_UNITS[self.mScanType])
+        if (self.builder.get_object('mot2_start_entry').get_property("visible")) :
+            self.builder.get_object('mot2_step_size').set_text('Step size: ' + '%.3f ' % mot2size + SCAN_TYPE_MOT2_UNITS[self.mScanType])
+        else :
+            self.builder.get_object('mot2_step_size').set_text('')            
 
 
     def calc_mot1_step_size(self):
@@ -371,7 +418,7 @@ class DiffControl:
         if resp == gtk.RESPONSE_OK:
             self.mLocalSavePath = chooser.get_filename()
             self.mServerSidePath,self.mSavePathOK = mountpoint_conversion.get_pilatus_path(self.mLocalSavePath)
-            self.update_summary_fields()
+            self.update_view()
             
             print self.mLocalSavePath, 'selected'
             
@@ -386,14 +433,14 @@ class DiffControl:
         # Start a thread to do the scanning
     
         # activate the stop scan and deactivate the start scan button
-        self.update_control_buttons()
+        self.update_view()
         
     def stop_scan(self, widget, data = None):
         self.mScanRunning = False
         
         # Stop the scan thread
         2
-        self.update_control_buttons()
+        self.update_view()
         
 
     def update_control_buttons(self):
@@ -408,7 +455,7 @@ class DiffControl:
     def scanTypeChanged(self,combobox):
         print 'scanTypeChanged'
         self.mScanType = combobox.get_active()
-        self.set_labels_and_hideshow_fields()
+        self.update_view()
               
 # If the program is run directly or passed as an argument to the python
 # interpreter then create a HelloWorld instance and show it
